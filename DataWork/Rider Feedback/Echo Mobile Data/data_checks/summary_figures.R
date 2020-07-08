0f89f3456a28657496d2698ff28f41b05dce03ef
Rob Marty <ramarty@email.wm.edu>
2020-07-08 17:37:18 -0400

    figures

--- DataWork/Rider Feedback/Echo Mobile Data/data_checks/summary_figures.R
+++ DataWork/Rider Feedback/Echo Mobile Data/data_checks/summary_figures.R
@@ -17,6 +17,21 @@ data$MATATU.NUMBER._L <- data$MATATU.NUMBER._L %>% tolower() %>% str_squish()
 data$matatu_num <- data$MATATU.NUMBER._L
 data$matatu_num_correct <- data$matatu_num %in% paste0(20:31)
 
+## Factors
+data$DRIVER.RATING._L <- data$DRIVER.RATING._L %>% str_squish() %>%
+  factor(levels = c("Very Safe", "Safe", "Unsafe", "Very Unsafe"))
+
+data$SPEED.RATING._L <- data$SPEED.RATING._L %>% str_squish() %>%
+  factor(levels = c("Too slow", "Okay", "Fast", "Dangerously Fast"))
+
+data$MATATU.OCCUPANCY_L <- data$MATATU.OCCUPANCY_L %>% str_squish() %>%
+  factor(levels = c("More people than can fit", "More people than seats", "Same number of people as seats", "Less people than seats"))
+
+data$PASSENGER.HAND.SANITISATION_L <- data$PASSENGER.HAND.SANITISATION_L %>% str_squish() %>%
+  factor(levels = c("Yes, effective", "Yes, but seemed limited", "Same number of people as seats", "No"))
+
+data$WHEREGOING_R %>% table() %>% View()
+
 # Subset -----------------------------------------------------------------------
 data <- data %>%
   filter(date >= "2020-06-26")
@@ -131,4 +146,122 @@ data_comp %>%
   labs(title = "June 28 Onwards") +
   ggsave(filename = file.path(echo_figures, "v22_by_10min_interval_not2627.png"),
          height = 4, width =12)
+
+# Answer to Questions ----------------------------------------------------------
+data_comp %>%
+  group_by(DRIVER.RATING._L) %>%
+  summarise(N = n()) %>%
+  ggplot(aes(x = DRIVER.RATING._L, y = N)) +
+  geom_col(fill = "dodgerblue4") +
+  geom_text(aes(label = N), nudge_y = 30) +
+  labs(x="", y="",
+       title = "How would you rate your matatu driver?") +
+  theme_minimal() +
+    ggsave(filename = file.path(echo_figures, "matatu_driver_rate.png"),
+           height = 3, width =6)
+
+data_comp %>%
+  group_by(SPEED.RATING._L) %>%
+  summarise(N = n()) %>%
+  ggplot(aes(x = SPEED.RATING._L, y = N)) +
+  geom_col(fill = "dodgerblue4") +
+  geom_text(aes(label = N), nudge_y = 40) +
+  labs(x="", y="",
+       title = "How would you describe your driver's speed?") +
+  theme_minimal() +
+  ggsave(filename = file.path(echo_figures, "matatu_speed_rate.png"),
+         height = 3, width =6)
+
+data_comp %>%
+  group_by(MATATU.OCCUPANCY_L) %>%
+  summarise(N = n()) %>%
+  ggplot(aes(x = MATATU.OCCUPANCY_L, y = N)) +
+  geom_col(fill = "dodgerblue4") +
+  geom_text(aes(label = N), nudge_y = 50) +
+  labs(x="", y="",
+       title = "How many passengers are on the matatu?") +
+  theme_minimal() +
+  coord_flip() +
+  ggsave(filename = file.path(echo_figures, "matatu_occupancy_rate.png"),
+         height = 3, width =6)
+
+data_comp %>%
+  group_by(PASSENGER.HAND.SANITISATION_L) %>%
+  summarise(N = n()) %>%
+  ggplot(aes(x = PASSENGER.HAND.SANITISATION_L, y = N)) +
+  geom_col(fill = "dodgerblue4") +
+  geom_text(aes(label = N), nudge_y = 50) +
+  labs(x="", y="",
+       title = "Did the matatu take measures to prevent spread of COVID?") +
+  theme_minimal() +
+  ggsave(filename = file.path(echo_figures, "matatu_covidmeasure_rate.png"),
+         height = 3, width =6)
+
+# Answer to Questions - By Vehicle ---------------------------------------------
+data_comp_veh <- data_comp %>%
+  filter(matatu_num_correct %in% T) %>%
+  group_by(matatu_num) %>%
+  mutate(N_veh = n()) %>%
+  ungroup() 
+
+data_comp_veh %>%
+  group_by(DRIVER.RATING._L, matatu_num, N_veh) %>%
+  summarise(N = n()) %>%
+  mutate(N = N / N_veh) %>%
+  ggplot(aes(x = matatu_num, y = N, group=DRIVER.RATING._L, fill=DRIVER.RATING._L)) +
+  geom_col(color="black") +
+  labs(x="", y="",
+       title = "How would you rate your matatu driver?") +
+  theme_minimal() +
+  coord_flip() +
+  labs(x = "Matatu Number", y = "Proportion", fill = "") +
+  ggsave(filename = file.path(echo_figures, "matatu_driver_rate_byveh.png"),
+         height = 3, width =6)
+
+data_comp_veh %>%
+  group_by(SPEED.RATING._L, matatu_num, N_veh) %>%
+  summarise(N = n()) %>%
+  mutate(N = N / N_veh) %>%
+  ggplot(aes(x = matatu_num, y = N, group=SPEED.RATING._L, fill=SPEED.RATING._L)) +
+  geom_col(color="black") +
+  labs(x="", y="",
+       title = "How would you describe your driver's speed?") +
+  theme_minimal() +
+  coord_flip() +
+  labs(x = "Matatu Number", y = "Proportion", fill = "") +
+  ggsave(filename = file.path(echo_figures, "matatu_speed_rate_byveh.png"),
+         height = 3, width =6)
+
+data_comp_veh %>%
+  group_by(MATATU.OCCUPANCY_L, matatu_num, N_veh) %>%
+  summarise(N = n()) %>%
+  mutate(N = N / N_veh) %>%
+  ggplot(aes(x = matatu_num, y = N, group=MATATU.OCCUPANCY_L, fill=MATATU.OCCUPANCY_L)) +
+  geom_col(color="black") +
+  labs(x="", y="",
+       title = "How many passengers are on the matatu?") +
+  theme_minimal() +
+  coord_flip() +
+  labs(x = "Matatu Number", y = "Proportion", fill = "") +
+  ggsave(filename = file.path(echo_figures, "matatu_occupancy_rate_byveh.png"),
+         height = 3, width =6)
+
+
+data_comp_veh %>%
+  group_by(PASSENGER.HAND.SANITISATION_L, matatu_num, N_veh) %>%
+  summarise(N = n()) %>%
+  mutate(N = N / N_veh) %>%
+  ggplot(aes(x = matatu_num, y = N, group=PASSENGER.HAND.SANITISATION_L, fill=PASSENGER.HAND.SANITISATION_L)) +
+  geom_col(color="black") +
+  labs(x="", y="",
+       title = "Did the matatu take measures to prevent spread of COVID?") +
+  theme_minimal() +
+  coord_flip() +
+  labs(x = "Matatu Number", y = "Proportion", fill = "") +
+  ggsave(filename = file.path(echo_figures, "matatu_covidmeasure_rate_byveh.png"),
+         height = 3, width =6)
+
+
+
+
 
