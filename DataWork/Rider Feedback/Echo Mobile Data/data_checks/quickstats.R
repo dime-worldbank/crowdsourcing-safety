bd5bdef9d624cb7b38d59e3a7a3a169f5cbfe84c
Rob Marty <ramarty@email.wm.edu>
2020-08-04 10:17:56 -0400

    addn checks

--- DataWork/Rider Feedback/Echo Mobile Data/data_checks/quickstats.R
+++ DataWork/Rider Feedback/Echo Mobile Data/data_checks/quickstats.R
@@ -28,15 +28,28 @@ data$MATATU.AMENITIES._R
 data$start_date %>% as.Date() %>% table()
 
 
+data <- readRDS(file.path(dropbox_file_path, "Data", "Rider Feedback", "Echo Mobile Data", "RawData", 
+                          "echo_data.Rds"))
+
 
 data <- data[data$how_identif %in% "reg no",]
 data$MATATU.NUMBER._R <- data$MATATU.NUMBER._R %>% tolower() %>% str_replace_all(" ", "") 
-data <- data[data$MATATU.NUMBER._R %in% "kbm199t",]
-data$start_date_r <- data$start_date %>% ymd_hm() %>% round_date(unit = "hour")
+data <- data[data$MATATU.NUMBER._R %in% "kbm119t",]
+data$start_date_r <- data$start_date %>% ymd_hm() %>% round_date(unit = "10 minutes")
 
-data <- data %>%
+data_s <- data %>%
   group_by(start_date_r) %>%
   dplyr::summarise(N = n())
+
+aa <- data[data$start_date_r %in% "2020-07-31 17:50:00",]
+
+
+
+ggplot(data_s) +
+  geom_col(data = data_s, 
+           aes(x = start_date_r, y= N),
+           color = "black") +
+  theme_minimal()
 
 
 kbm119t
