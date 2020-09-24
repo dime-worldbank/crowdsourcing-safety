a5e8e446efaaf4d80cbe631746eb324b90d9fc29
Rob Marty <ramarty@email.wm.edu>
2020-09-24 17:56:15 -0400

    update

--- DataWork/Rider Feedback/All Data/clean_data/append_clean_data.R
+++ DataWork/Rider Feedback/All Data/clean_data/append_clean_data.R
@@ -1,14 +1,139 @@
 # PSV Rider Feedback: Clean Data
 
 # Load Data --------------------------------------------------------------------
-data <- readRDS(file.path(dropbox_file_path, "Data", "Rider Feedback", "Echo Mobile Data", "RawData", 
-                          "echo_data.Rds"))
+## Shortcode Data
+sc_data <- readRDS(file.path(dropbox_file_path, "Data", "Rider Feedback", "Echo Mobile Data", "RawData", 
+                             "echo_data.Rds"))
 
-charc_df <- read_xlsx(file.path(dropbox_file_path, "Data", "Matatu Data", 
-                                "Characteristics", "RawData", "vehicle_basic_info.xlsx"))
-charc_df <- charc_df %>%
-  mutate(plate      = plate %>% str_replace_all(" ", "") %>% tolower(),
-         psv_number = psv_number %>% str_replace_all(" ", "") %>% tolower())
+## QR Data
+qr_data <- readRDS(file.path(dropbox_file_path, "Data", "Rider Feedback", "PSV Safety Systems Data", "RawData", 
+                             "qr_data.Rds"))
+
+## Sticker Data
+sticker_df <- read_xlsx(file.path(dropbox_file_path, "Data", "Matatu Data", 
+                                  "Sticker Information", "psv_sticker_information.xlsx"))
+
+# SC Data ----------------------------------------------------------------------
+sc_data <- sc_data %>%
+  dplyr::select(start_date, invite_date, complete_date,
+                MATATU.NUMBER._L,
+                DRIVER.RATING._L,
+                SPEED.RATING._L,
+                MATATU.OCCUPANCY_L,
+                PASSENGER.HAND.SANITISATION_L,
+                WHEREGOING_L,
+                FEEDBACK_R,
+                
+                MATATU.NUMBER._L_asked,
+                DRIVER.RATING._L_asked,
+                SPEED.RATING._L_asked,
+                MATATU.OCCUPANCY_L_asked,
+                PASSENGER.HAND.SANITISATION_L_asked,
+                WHEREGOING_L_asked,
+                FEEDBACK_R_asked,
+                phone_hash,
+                file) %>%
+  mutate(response_method = "shortcode")
+
+## Clean names
+names(sc_data) <- names(sc_data) %>% 
+  tolower() %>% 
+  str_replace_all("._r|._l", "") %>%
+  str_replace_all("\\.", "_")
+
+names(sc_data) <- names(sc_data) %>%
+  str_replace_all("matatu_occupanc", "occupancy") %>%
+  str_replace_all("passenger_hand_sanitisatio", "covid_measures") %>%
+  str_replace_all("wheregoin", "where_going") %>%
+  str_replace_all("feedbac", "feedback") %>%
+  str_replace_all("matatu_no", "matatu_number")
+
+## Complete Date - Add Seconds
+sc_data$complete_date <- paste0(sc_data$complete_date, ":00")
+sc_data$complete_date[sc_data$complete_date %in% ":00"] <- ""
+
+# QR Data ----------------------------------------------------------------------
+## Clean Names
+names(qr_data) <- names(qr_data) %>%
+  str_replace_all("How would you rate your matatu driver\\?", "driver_rating") %>%
+  str_replace_all("Did the matatu take measures to help prevent the spread of the coronavirus\\? For example, limiting the number of people in the matatu or providing sanitary wipes\\?", "covid_measures") %>%
+  str_replace_all("How would you describe your driver’s speed\\?", "speed_rating") %>%
+  str_replace_all("How many passengers are on the matatu\\?", "occupancy") %>%
+  str_replace_all("When choosing a matatu, how important are amenities \\(eg, music, wifi\\)\\?", "amenity_importance") %>%
+  str_replace_all("Leave any comments about the matatu or ride. For example, report unsafe driving or compliment the driver.", "feedback")
+
+qr_data <- qr_data %>%
+  dplyr::rename(complete_date = Created,
+                qr_web_address = Source)
+
+## Select Variables
+qr_data <- qr_data %>%
+  dplyr::select(complete_date, driver_rating, driver_rating_asked,
+                covid_measures, covid_measures_asked,
+                speed_rating, speed_rating_asked,
+                occupancy, occupancy_asked,
+                amenity_importance, amenity_importance_asked,
+                feedback, feedback_asked,
+                qr_web_address,
+                file) %>%
+  mutate(response_method = "qr code")
+
+## Remove testing
+qr_data$feedback <- qr_data$feedback %>% tolower() %>% str_squish()
+qr_data <- qr_data[!(qr_data$feedback %in% c("test", "testing")),]
+
+# Append and standardize variables ---------------------------------------------
+data <- bind_rows(qr_data, sc_data)
+
+data <- data %>%
+  dplyr::rename(complete_datetime = complete_date) %>%
+  mutate(complete_datetime = complete_datetime %>% ymd_hms) %>%
+  mutate(complete_date = complete_datetime %>% as.Date)
+
+data <- data %>%
+  mutate_if(is.character, tolower) %>%
+  mutate_if(is.character, str_squish)
+
+data$covid_measures[data$covid_measures %in% "yes - but measures are limited"] <- "yes, but seemed limited"
+data$covid_measures[data$covid_measures %in% "yes - but measures are limited - no"] <- "yes, but seemed limited"
+
+data$speed_rating[data$speed_rating %in% "much too fast - speed seemed to put my life in danger"] <- "dangerously fast"
+data$speed_rating[data$speed_rating %in% "too fast - felt unsafe"] <- "fast"
+data$speed_rating[data$speed_rating %in% "fine - drove at appropriate & safe speed"] <- "okay"
+
+data$occupancy[data$occupancy %in% "there are less people than seats."] <- "less people than seats"
+data$occupancy[data$occupancy %in% "there are more people than can fit (people hanging off side)"] <- "more people than can fit"
+data$occupancy[data$occupancy %in% "there are more people than seats"] <- "more people than seats"
+data$occupancy[data$occupancy %in% "there are the same number of people as seats"] <- "same number of people as seats"
+
+for(var in names(data) %>% str_subset("_asked")){
+  data[[var]][!(data[[var]] %in% "yes")] <- "no"
+}
+
+# Clean Sticker Data -----------------------------------------------------------
+sticker_df <- sticker_df %>%
+  mutate(plate      = plate %>% tolower %>% str_squish %>% str_replace_all(" ", ""),
+         psv_number = psv_number %>% tolower %>% str_squish %>% str_replace_all(" ", "")) %>%
+  dplyr::rename(matatu_number = psv_number)
+
+# License Plate Number ---------------------------------------------------------
+#### Matatu number, numeric
+data$matatu_number <- data$matatu_number %>%
+  str_replace_all("psv no", "") %>%
+  str_replace_all("psv", "") %>%
+  str_replace_all("[[:punct:]]", "") %>%
+  str_squish() %>%
+  str_replace_all(" ", "")
+    
+psv_no_df <- sticker_df %>%
+  dplyr::select(matatu_number, plate) %>%
+  dplyr::rename(plate_1 = plate)
+
+data <- merge(data, psv_no_df, by = "matatu_number", all.x = T, all.y = F)
+
+
+
+
 
 # Select variables and clean names ---------------------------------------------
 ## Select useful variables
