c4b1ea9b192ad83d322c802b310dc438e183e10b
meyharmohd <meyhar.mohammed@gmail.com>
2020-12-13 14:29:26 -0500

    Updated MM  file path

--- _psv_feedback_master.R
+++ _psv_feedback_master.R
@@ -4,10 +4,16 @@
 #### Main
 if(Sys.info()[["user"]] == "robmarty") dropbox_file_path <- "~/Dropbox/World Bank/IEs/PSV Rider Feedback"
 if(Sys.info()[["user"]] == "WB521633") dropbox_file_path <- "C:/Users/wb521633/Dropbox/World Bank/IEs/PSV Rider Feedback"
+<<<<<<< Updated upstream
 if(Sys.info()[["user"]] == "meyhar") dropbox_file_path <- "/Users/meyhar/Dropbox/PSV Rider Feedback"
 
 
+=======
+>>>>>>> Stashed changes
 if(Sys.info()[["user"]] == "WB521633") onedrive_file_path <- "C:/Users/wb521633/OneDrive - WBG/PSV Rider Feedback"
+
+## MM file path
+if(Sys.info()[["user"]] == "meyhar") dropbox_file_path <- "~/Dropbox/PSV Rider Feedback"
 
 #### From Main
 echo_figures <- file.path(dropbox_file_path, "Data", "Rider Feedback", "Echo Mobile Data", "Outputs", "figures")
