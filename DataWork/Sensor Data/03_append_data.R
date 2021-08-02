36f35b6051b677b9f04f72752d5cd6bdb04a8fa3
Rob Marty <ramarty@email.wm.edu>
2021-08-02 19:10:45 -0400

    append wialon data

--- DataWork/Sensor Data/03_append_data.R
+++ DataWork/Sensor Data/03_append_data.R
@@ -12,7 +12,7 @@ clean_dates <- function(x){
 
 # Echo Driving -----------------------------------------------------------------
 #### Load Data
-echodriving <- file_dir <- file.path(sensors_dir, "RawData", "echo_driving_individual_data") %>%
+echodriving <- file.path(sensors_dir, "RawData", "echo_driving_individual_data") %>%
   list.files(pattern = "*.gz.parquet",
              full.names = T) %>%
   map_df(function(path) read_parquet(path))
@@ -24,96 +24,36 @@ echodriving_clean <- echodriving %>%
   dplyr::mutate_at(vars(value, max_speed, distance), ~.x %>% 
                      str_replace_all("[[:alpha:]]|[[/]]", "") %>%
                      str_squish() %>%
-                     as.numeric)
+                     as.numeric) %>%
+  dplyr::rename(max_speed_kmhr = max_speed,
+                distance_km = distance,
+                value_g = value,
+                begin_datetime = begin_datetime_str,
+                end_datetime = end_datetime_str)
 
-echodriving_clean$reg_no[1:10] %>% sub_string(-1,-8)
+#### Save Data
+write_parquet(echodriving_clean, file.path(sensors_dir, "FinalData", "echodriving.gz.parquet"), 
+              compression = "gzip", compression_level = 5)
 
+# Sensor Tracing ---------------------------------------------------------------
+#### Load Data
+sensortracing <- file_dir <- file.path(sensors_dir, "RawData", "sensor_tracing_individual_data") %>%
+  list.files(pattern = "*.gz.parquet",
+             full.names = T,
+             recursive = T) %>%
+  map_df(function(path) read_parquet(path))
 
-                value = value %>% str_replace_all(" g", "") %>% str_squish() %>% as.numeric,
-                max_speed = max_speed %>% str_replace_all(" km/h", "") %>% str_squish() %>% as.numeric,
-                distance = distance %>% str_replace_all(" km", "") %>% str_squish() %>% as.numeric)
+#### Clean Data
+sensortracing_clean <- sensortracing %>%
+  dplyr::mutate(time_str = time_str %>% clean_dates()) %>%
+  dplyr::mutate_at(vars(speed), ~.x %>% 
+                     str_replace_all("[[:alpha:]]|[[/]]", "") %>%
+                     str_squish() %>%
+                     as.numeric) %>%
+  dplyr::rename(speed_kmhr = speed) %>%
+  dplyr::select(-diff_num_vars_rawdata)
 
-
-echodriving$distance
-
-# Parameters -------------------------------------------------------------------
-# If should skip data already scraped (checking vehicle and day). 
-# If TRUE, re-scrapes data
-# If FALSE, skips data already scraped
-OVERWRITE_DATA_SENSORTRACING <- T
-OVERWRITE_DATA_ECHODRIVING   <- T
-
-# Load Token/IDS ---------------------------------------------------------------
-# Load token associated with account and IDs (numeric IDs for vehicles, reports
-# and resource)
-wialon_token <- read.table(file.path(sensors_dir, "wialon_token", "wialon_token.txt"), stringsAsFactors = F)$V1
-
-# Regenerate IDs - Needed in case additional vehicles added
-source(file.path(github_dir, "DataWork", "Sensor Data", "01_get_wialon_ids.R")) 
-
-users_df    <- read.csv(file.path(sensors_dir, "RawData", "wialon_ids", "user_ids.csv"))
-resource_df <- read.csv(file.path(sensors_dir, "RawData", "wialon_ids", "resource_ids.csv"))
-report_df   <- read.csv(file.path(sensors_dir, "RawData", "wialon_ids", "report_ids.csv"))
-
-resource_id    <- resource_df$id
-
-# Dates to Scrape --------------------------------------------------------------
-# Don't scrape today; wait until the day is complete so get full data.
-
-scrape_until_date <- Sys.time() %>% with_tz(tzone = "UTC") %>% as.Date()
-scrape_until_date <- scrape_until_date - 1
-
-dates <- seq(from = ymd("2021-07-01"),
-             to = scrape_until_date,
-             by = 1
-) %>%
-  as.character()
-
-# Download Echo Driving Report -------------------------------------------------
-for(date_i in dates){
-  print(date_i)
-  
-  file_name <- paste0("echodriving_", date_i, ".gz.parquet")
-  file_dir <- file.path(sensors_dir, "RawData", "echo_driving_individual_data", file_name)
-  
-  if(!file.exists(file_dir) | OVERWRITE_DATA_ECHODRIVING){
-    df_out <- map_df(unique(users_df$id), function(user_id_i){
-      get_report(user_id = user_id_i, 
-                 report_id = report_df$id[report_df$n %in% "Eco Driving Report"], 
-                 resource_id = resource_id, 
-                 datetime_begin = paste(date_i, "00:00:00") %>% ymd_hms(tz = "UTC") %>% as.numeric(), 
-                 datetime_end = paste(date_i, "23:59:59") %>% ymd_hms(tz = "UTC") %>% as.numeric(),
-                 wialon_token,
-                 users_df)
-    })
-    
-    write_parquet(df_out, file_dir, compression = "gzip", compression_level = 5)
-  }
-  
-}
-
-# Download Sensor Tracing Data -------------------------------------------------
-for(date_i in dates){
-  print(date_i)
-  for(user_id_i in unique(users_df$id)){
-    
-    file_name <- paste0("sensortracing_", user_id_i, "_", date_i, ".gz.parquet")
-    dir.create(file.path(sensors_dir, "RawData", "sensor_tracing_individual_data", date_i))
-    file_dir <- file.path(sensors_dir, "RawData", "sensor_tracing_individual_data", date_i, file_name)
-    
-    if(!file.exists(file_dir) | OVERWRITE_DATA_SENSORTRACING){
-      
-      df_out <- get_report(user_id = user_id_i, 
-                           report_id = report_df$id[report_df$n %in% "Units Sensors Tracing - all"], 
-                           resource_id = resource_id, 
-                           datetime_begin = paste(date_i, "00:00:00") %>% ymd_hms(tz = "UTC") %>% as.numeric(), 
-                           datetime_end = paste(date_i, "23:59:59") %>% ymd_hms(tz = "UTC") %>% as.numeric(),
-                           wialon_token,
-                           users_df)
-      
-      write_parquet(df_out, file_dir, compression = "gzip", compression_level = 5)
-    }
-    
-  }
-}
+#### Save Data
+write_parquet(echodriving_clean, file.path(sensors_dir, "FinalData", "sensortracing.gz.parquet"), 
+              compression = "gzip", compression_level = 5)
 
