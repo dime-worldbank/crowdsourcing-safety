ba2efd60af1a59cf3be052364c72bd871faab384
Rob Marty <ramarty@email.wm.edu>
2021-10-28 10:20:00 -0400

    aggregate polylines to daily level

--- DataWork/Sensor Data/06_aggregate_to_daily.R
+++ DataWork/Sensor Data/06_aggregate_to_daily.R
@@ -23,26 +23,85 @@ sensor_nonsum_df <- sensor_df %>%
 
 sensor_sum_df <- sensor_df %>%
   group_by(reg_no, reg_no_id, regno_clean, sacco, route, date) %>%
-  dplyr::summarise_at(vars(contains("N_")),
+  dplyr::summarise_at(vars(matches("\\bN_")),
                       sum)
 
 sensor_agg_df <- merge(sensor_nonsum_df, sensor_sum_df, 
                        by = c("reg_no", "reg_no_id", "regno_clean", "sacco", "route", "date"))
 
-# Export Data Only -------------------------------------------------------------
+# Aggregate Polylines ----------------------------------------------------------
+# Using method of: summarize(geometry = st_union(geometry)) was inefficient, so
+# using alternate method.
+
+#### Add group_id to merge back into
+sensor_agg_df <- sensor_agg_df %>%
+  dplyr::mutate(group_id = paste(reg_no_id, date))
+
+# Creating polylines doesn't work if only one point in linestring or if empty;
+# we remove those before aggregating polylines
+sensor_lines_sf <- sensor_sf %>%
+  dplyr::filter(!is.na(distance_km),
+                distance_km > 0) %>%
+  arrange(hour) %>% # Need to arrange as need ordered coordinates at daily leel
+  dplyr::mutate(group_id = paste(reg_no_id, date)) 
+
+#### Aggregate Polylines
+counter_i <<- 1 # to show progress
+daily_polyline_sf <- lapply(unique(sensor_lines_sf$group_id), function(group_id_i){
+  if((counter_i %% 100) == 0) print(paste0("Aggregating polylines to daily level: ", 
+                                           counter_i, 
+                                           " / ", 
+                                           length(unique(sensor_lines_sf$group_id))))
+  
+  ## Grab polyline for vehicle on date
+  sensor_lines_sf_i <- sensor_lines_sf[sensor_lines_sf$group_id %in% group_id_i,]
+  
+  ## Grab coordinates as dataframe
+  coords_df <- sensor_lines_sf_i$geometry %>% 
+    st_coordinates() %>%
+    as.data.frame()
+  coords_df$group_id <- group_id_i
+  
+  ## Make polyline
+  polyline_sf <- sf_linestring(coords_df, 
+                               x = "X", 
+                               y = "Y",
+                               linestring_id = "group_id")
+  st_crs(polyline_sf) <- 4326 
+  
+  ## Add total distance between min/max lat & lon
+  bbox_df <- st_bbox(polyline_sf) %>%
+    as.matrix() %>%
+    t() %>%
+    as.data.frame()
+  
+  polyline_sf$distance_minmax_latlon_daily_km = distHaversine(p1 = c(bbox_df$xmin,
+                                                                     bbox_df$ymin),
+                                                              p2 = c(bbox_df$xmax,
+                                                                     bbox_df$ymax))/1000
+  
+  counter_i <<- counter_i + 1
+  return(polyline_sf)
+}) %>% 
+  bind_rows()
+
+#### Add Distance Variables
+daily_polyline_sf$distance_daily_km <- as.numeric(st_length(daily_polyline_sf))/1000 
+
+#### Merge Back to Main Dataframes
+sensor_agg_sf <- sensor_agg_df %>%
+  left_join(daily_polyline_sf, by = "group_id")
+
+sensor_agg_df <- sensor_agg_sf
+sensor_agg_df$geometry <- NULL
+
+# Export -----------------------------------------------------------------------
+## Data Only
 write_parquet(sensor_agg_df, file.path(sensors_dir, "FinalData", "sensor_day.gz.parquet"), 
               compression = "gzip", compression_level = 5)
 saveRDS(sensor_agg_df, file.path(sensors_dir, "FinalData", "sensor_day.Rds"))
 
-# Aggregate Polylines ----------------------------------------------------------
-sensor_polylineagg_sf <- sensor_sf %>%
-  group_by(reg_no, reg_no_id, regno_clean, sacco, route, date) %>%
-  summarize(geometry = st_union(geometry))
-
-sensor_agg_sf <- right_join(sensor_polylineagg_sf,
-                           sensor_agg_df, 
-                           by = c("reg_no", "reg_no_id", "regno_clean", "sacco", "route", "date"))
-
+## Data with Polyline
 saveRDS(sensor_agg_sf, file.path(sensors_dir, "FinalData", "sensor_day_polyline.Rds"))
 
 
