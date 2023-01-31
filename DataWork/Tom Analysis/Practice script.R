8320f7e434f4e0786d68a4db2ea58c7b9e3e30a2
Tom-Harris-MIT <100818286+Tom-Harris-MIT@users.noreply.github.com>
2023-01-31 18:55:51 +0000

    Update Practice script.R

--- DataWork/Tom Analysis/Practice script.R
+++ DataWork/Tom Analysis/Practice script.R
@@ -4,21 +4,21 @@
 #### Before any new scripts, open and run the master script! Sets all root directories
 
 
-df <- readRDS(
-  file.path(sensors_dir, "FinalData", "sensor_day.Rds"))
+df <- readRDS(file.path(sensors_dir, "FinalData", "sensor_day.Rds"))
 
 
-df_2 <- readRDS(
-  file.path(rider_feedback_dir, "FinalData", "rider_feedback.Rds"))
+df_2 <- readRDS(file.path(rider_feedback_dir, "FinalData", "rider_feedback.Rds"))
 
-df_2_trans <- readRDS(
-  file.path(rider_feedback_dir, "FinalData", "rider_feedback_translated.Rds"))
+df_2_trans <- readRDS(file.path(
+  rider_feedback_dir,
+  "FinalData",
+  "rider_feedback_translated.Rds"
+))
 
 
 ## common id is probs regno_clean
 
 ## first step, aggregate everything to the vehicle level
-#how safe does sensor data say vehicle, how safe does feedback data say it is.... do these corr? 
-# how do we define 'safe' . e,g, avg speed, make otehr indicators .... proportion of time spent over 80 kmh etc. 
-# eventually some kidn of index of dangerous driving .... 
-
+#how safe does sensor data say vehicle, how safe does feedback data say it is.... do these corr?
+# how do we define 'safe' . e,g, avg speed, make other indicators .... proportion of time spent over 80 kmh etc.
+# eventually some kidn of index of dangerous driving ....
