11fba5964ac2ca1d2c2debb1d2d9d0497877cd66
Tom-Harris-MIT <100818286+Tom-Harris-MIT@users.noreply.github.com>
2023-02-03 12:04:16 +0000

    Update Grouping feedback data.R

--- DataWork/Tom Analysis/Grouping feedback data.R
+++ DataWork/Tom Analysis/Grouping feedback data.R
@@ -23,11 +23,12 @@ sensor_data$sacco <- sensor_data$sacco.x
 feedback_data <-
   readRDS(file.path(rider_feedback_dir, "FinalData", "rider_feedback.Rds"))
 
-feedback_trans_data <- readRDS(file.path(
-  rider_feedback_dir,
-  "FinalData",
-  "rider_feedback_translated.Rds"
-))
+## Looks like the translated data isn't there anymore...?
+# feedback_trans_data <- readRDS(file.path(
+#   rider_feedback_dir,
+#   "FinalData",
+#   "rider_feedback_translated.Rds"
+# ))
 
 
 ### Grouping the feedback data
@@ -48,6 +49,9 @@ feedback_data$safety_numeric <-
     to = c(4, 3, 2, 1)
   )
 
+feedback_data$safety_numeric <-
+  as.numeric(feedback_data$safety_numeric)
+
 # converting speed feedback to numeric
 
 feedback_data$speed_numeric <- feedback_data$speed_label_en
@@ -56,24 +60,27 @@ feedback_data$speed_numeric <-
   mapvalues(
     feedback_data$speed_numeric,
     from = c(
-      "Very slow [0 - 10 km/h]",
-      "Slow [10 - 30]",
-      "Average [30 - 50]",
-      "Fast [50 - 80]",
-      "Very fast [80k]"
+      "Very slow [0-10 km/h]",
+      "Slow [10-30]",
+      "Average [30-50]",
+      "Fast [50-80]",
+      "Very fast [80+]"
     ),
     to = c(1, 2, 3, 4, 5)
   )
 
+feedback_data$speed_numeric <-
+  as.numeric(feedback_data$speed_numeric)
+
 
 # Group the data by the "regno_clean" id variable
-grouped_data <- sensor_data %>%
+grouped_data <- feedback_data %>%
   group_by(regno_clean)
 
-# Need a variable which measures number of violations per km, otherwise
-# biased by buses which drive longer routes.
-grouped_data$over_80_by_km <-
-  grouped_data$N_speed_over_80 / grouped_data$distance_km
+# # Need a variable which measures number of violations per km, otherwise
+# # biased by buses which drive longer routes.
+# grouped_data$over_80_by_km <-
+#   grouped_data$N_speed_over_80 / grouped_data$distance_km
 
 # ### Specify which columns we want to take weighted average of:
 variablesToAvg <-
@@ -97,3 +104,43 @@ final_data <-
 
 final_data$sacco <-
   grouped_data$sacco[match(final_data$regno_clean, grouped_data$regno_clean)]
+
+
+
+
+
+
+
+
+
+
+
+
+# Q. does safety rating correlate with speed rating?
+
+# plot of number of 80kmh speed violations per km by Sacco
+plot_1 <- ggplot(feedback_data) +
+  aes(
+    x = feedback_data$safety_numeric,
+    y = feedback_data$speed_numeric,
+    colour = feedback_data$matatu_sacco_stk_inst_srvy
+  ) +
+  geom_point(shape = "circle",
+             size = 3,
+             alpha = 0.6) +
+  scale_color_hue(direction = 1) +
+  labs(
+    x = "asdf",
+    y = "asdf",
+    title = "asdf",
+    subtitle = " "
+  ) +
+  theme_minimal() +
+  theme(legend.position = "top") +
+  labs(fill = "Sacco Company")
+
+plot_1
+
+# interactive version
+ggplotly(plot_1)
+
