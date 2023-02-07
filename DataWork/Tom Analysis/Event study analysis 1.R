57ca2b3a94d9ed02baf5aae4eb5dc341958eae80
Tom-Harris-MIT <100818286+Tom-Harris-MIT@users.noreply.github.com>
2023-02-07 23:25:17 +0000

    Update Event study analysis 1.R

--- DataWork/Tom Analysis/Event study analysis 1.R
+++ DataWork/Tom Analysis/Event study analysis 1.R
@@ -70,6 +70,7 @@ analysis_data <- sensor_data_clean %>%
     date = as.factor(date)
   )
 
+#### Speed violations event study ####
 
 # Regression results
 
@@ -221,3 +222,158 @@ ggplot(fig_data, aes(x = label, y = coef)) +
     legend.text = element_text(size = 10)
   ) +
   ggtitle("Event study, >80km/h violations per km")
+
+
+#### G violations event study ####
+
+# Regression results
+
+res_es <-
+  felm(total_g_violations ~ factor(days_since_sticker) |
+         regno_clean + date | 0 | regno_clean,
+       data = analysis_data)
+res_es
+
+stargazer(res_es, type = 'text')
+
+res_dind <-
+  felm(total_g_violations ~ after |
+         regno_clean + date | 0 | regno_clean,
+       data = analysis_data)
+res_dind
+
+stargazer(res_dind, type = 'text')
+
+
+# Variable label
+labels <- c()
+var_list <- c()
+for (num in seq(-30, 30)) {
+  if (num != -1) {
+    labels <- c(labels, num)
+    var_list <-
+      c(var_list,
+        paste0("factor(days_since_sticker)", as.character(num)))
+  }
+}
+
+# Figure data
+fig_data <- tibble(
+  label = labels,
+  es_coef = summary(res_es)$coef[var_list, "Estimate"] * 100,
+  es_se = summary(res_es)$coef[var_list, "Cluster s.e."] * 100,
+  dind_coef = summary(res_dind)$coef["afterTRUE", "Estimate"] * 100,
+  dind_se = summary(res_dind)$coef["afterTRUE", "Cluster s.e."] * 100
+) %>%
+  add_row(label = -1,
+          es_coef = 0,
+          es_se = 0) %>%
+  mutate(
+    dind_coef = ifelse(label >= 0, dind_coef, 0),
+    dind_se = ifelse(label >= 0, dind_se, 0)
+  )
+
+
+# Figure 1
+
+ggplot(fig_data, aes(x = label, y = es_coef)) +
+  geom_pointrange(
+    aes(ymin = es_coef - 1.96 * es_se, ymax = es_coef + 1.96 * es_se),
+    alpha = 0.7,
+    color = 'navy'
+  ) +
+  geom_vline(
+    xintercept = -0.5,
+    alpha = 0.3,
+    linetype = "dashed",
+    size = 0.3
+  ) +
+  geom_line(
+    data = fig_data,
+    aes(
+      x = label,
+      y = dind_coef,
+      group = (label >= 0)
+    ),
+    color = "red",
+    alpha = 0.7
+  ) +
+  geom_line(
+    data = fig_data,
+    aes(
+      x = label,
+      y = dind_coef + 1.96 * dind_se,
+      group = (label >= 0)
+    ),
+    linetype = "dashed",
+    color = "red",
+    alpha = 0.7
+  ) +
+  geom_line(
+    data = fig_data,
+    aes(
+      x = label,
+      y = dind_coef - 1.96 * dind_se,
+      group = (label >= 0)
+    ),
+    linetype = "dashed",
+    color = "red",
+    alpha = 0.7
+  ) +
+  theme_classic() +
+  geom_hline(yintercept = 0,
+             alpha = 0.5,
+             size = 0.5) +
+  ylab("Coefficient estimates & 95% CI") +
+  xlab("Days relative to installation of sticker") +
+  theme(axis.text = element_text(size = 12),
+        axis.title = element_text(size = 14)) +
+  ggtitle("Event study, G force violations")
+
+
+
+# Figure 2
+
+fig_data <- tibble(
+  label = labels,
+  coef = summary(res_es)$coef[var_list, "Estimate"] * 100,
+  se = summary(res_es)$coef[var_list, "Cluster s.e."] * 100
+) %>%
+  add_row(label = -1, coef = 0, se = 0)
+
+ggplot(fig_data, aes(x = label, y = coef)) +
+  geom_point() +
+  geom_ribbon(aes(
+    ymin = coef - 1.645 * se,
+    ymax = coef + 1.645 * se,
+    fill = "90%"
+  ),
+  alpha = 0.3) +
+  geom_ribbon(aes(
+    ymin = coef - 1.96 * se,
+    ymax = coef + 1.96 * se,
+    fill = "95%"
+  ), alpha = 0.2) +
+  geom_vline(
+    xintercept = -0.5,
+    alpha = 0.3,
+    linetype = "dashed",
+    size = 0.3
+  ) +
+  theme_classic() +
+  geom_hline(yintercept = 0,
+             alpha = 0.5,
+             size = 0.5) +
+  scale_fill_manual(name = "Confidence Intervals",
+                    values = c("90%" = "navy", "95%" = "blue")) +
+  guides(fill = guide_legend(override.aes = list(alpha = c(0.3, 0.2)))) +
+  ylab("Coefficient estimates & CIs") +
+  xlab("Days relative to sticker installation") +
+  theme(
+    axis.text = element_text(size = 12),
+    axis.title = element_text(size = 14),
+    legend.title = element_text(size = 12),
+    legend.text = element_text(size = 10)
+  ) +
+  ggtitle("Event study, G force violations")
+
