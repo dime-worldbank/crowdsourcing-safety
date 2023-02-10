a7ada8808d5ba66b7f83c22aa1b566fdfd0443f0
Tom-Harris-MIT <100818286+Tom-Harris-MIT@users.noreply.github.com>
2023-02-10 00:25:34 +0000

    Update Comparing grouped feedback and sensor data.R

--- DataWork/Tom Analysis/Comparing grouped feedback and sensor data.R
+++ DataWork/Tom Analysis/Comparing grouped feedback and sensor data.R
@@ -7,12 +7,14 @@
 if (!require("pacman")) {
   install.packages("pacman")
 }
-pacman::p_load(tidyr,
-               dplyr, plyr,
-               wesanderson,
-               plotly,
-               ggplot2,
-               data.table)
+pacman::p_load(
+  tidyr,
+  dplyr, plyr,
+  wesanderson,
+  plotly,
+  ggplot2,
+  data.table
+)
 
 # Loading data
 sensor_data <-
@@ -45,11 +47,13 @@ feedback_data$safety_numeric <- feedback_data$safety_label_en
 feedback_data$safety_numeric <-
   mapvalues(
     feedback_data$safety_numeric,
-    from = c("Not Very Safe",
-             "Not Safe",
-             "Safe",
-             "Very Safe"),
-    #I'm not sure here if 'not very safe' is better than 'not safe'?
+    from = c(
+      "Not Very Safe",
+      "Not Safe",
+      "Safe",
+      "Very Safe"
+    ),
+    # I'm not sure here if 'not very safe' is better than 'not safe'?
     to = c(4, 3, 2, 1)
   )
 
@@ -96,15 +100,16 @@ grouped_data <-
 
 # getting rid of rows where N_speed_over_0 is 'na' or zero as this doesn't make sense..
 grouped_data <- grouped_data %>%
-  filter(!is.na(safety_numeric) &  !is.na(speed_numeric))
+  filter(!is.na(safety_numeric) & !is.na(speed_numeric))
 
 
 ### collapse to bus using weighted means
 final_data <-
   grouped_data[,
-               lapply(.SD, mean), ## applies weighted mean function with weights w over specified columns
-               by = list(regno_clean),
-               .SDcols = variablesToAvg] ## specifies columns to average
+    lapply(.SD, mean), ## applies weighted mean function with weights w over specified columns
+    by = list(regno_clean),
+    .SDcols = variablesToAvg
+  ] ## specifies columns to average
 
 final_data$sacco <-
   grouped_data$matatu_sacco_stk_inst_srvy[match(final_data$regno_clean, grouped_data$regno_clean)]
@@ -121,9 +126,11 @@ plot_1 <- ggplot(feedback_grouped) +
     y = feedback_grouped$speed_numeric,
     color = sacco
   ) +
-  geom_point(shape = "circle",
-             size = 3,
-             alpha = 0.6) +
+  geom_point(
+    shape = "circle",
+    size = 3,
+    alpha = 0.6
+  ) +
   scale_color_hue(direction = 1) +
   labs(
     x = "Safety Rating (1 = Very safe, 4 = Very unsafe)",
@@ -156,12 +163,14 @@ ggplotly(plot_1)
 grouped_data <- sensor_data %>%
   group_by(regno_clean)
 
+#### note to self
+# readings not taken per x seconds - based on change in speed --- so divide by journey time
 # Need a variable which measures number of violations per km, otherwise
 # biased by buses which drive longer routes.
 grouped_data$over_80_by_km <-
   grouped_data$N_speed_over_80 / grouped_data$distance_km
 
-# ### Specify which columns we want to take weighted average of:
+# Specify which columns we want to take weighted average of:
 variablesToAvg <-
   names(grouped_data)[c(1, 2, 9:64, 73)] ## you may want to tweak which variables you average
 
@@ -177,9 +186,10 @@ grouped_data <- grouped_data %>%
 ### collapse to FB postcode level using weighted means
 final_data <-
   grouped_data[,
-               lapply(.SD, mean), ## applies weighted mean function with weights w over specified columns
-               by = list(regno_clean),
-               .SDcols = variablesToAvg] ## specifies columns to average
+    lapply(.SD, mean), ## applies weighted mean function with weights w over specified columns
+    by = list(regno_clean),
+    .SDcols = variablesToAvg
+  ] ## specifies columns to average
 
 # won't work as sacco variable no longer there...
 # final_data$sacco <-
@@ -194,6 +204,9 @@ joined_data <- left_join(sensor_grouped, feedback_grouped)
 
 
 # plot of sensor vs. feedback data
+# note to self
+# focus on proportion of responses that are fast or very fast instead of average of 1 to 5
+
 
 plot_1 <- ggplot(joined_data) +
   aes(
@@ -201,9 +214,11 @@ plot_1 <- ggplot(joined_data) +
     y = joined_data$speed_numeric,
     color = sacco
   ) +
-  geom_point(shape = "circle",
-             size = 3,
-             alpha = 0.6) +
+  geom_point(
+    shape = "circle",
+    size = 3,
+    alpha = 0.6
+  ) +
   scale_color_hue(direction = 1) +
   labs(
     x = ">80km/h violations per km",
