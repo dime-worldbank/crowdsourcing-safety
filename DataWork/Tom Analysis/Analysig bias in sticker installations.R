f1a1067bd86093ed3e654df93e56d7c569e0e7b9
Tom-Harris-MIT <100818286+Tom-Harris-MIT@users.noreply.github.com>
2023-03-21 21:41:37 +0000

    Update Analysig bias in sticker installations.R

--- DataWork/Tom Analysis/Analysig bias in sticker installations.R
+++ DataWork/Tom Analysis/Analysig bias in sticker installations.R
@@ -1,6 +1,6 @@
 # Tom Harris
 # Analysig bias in sticker installations
-# asdf
+# ...
 
 # n.b. Before any new scripts, run psv_feedback_master.R
 
@@ -71,12 +71,19 @@ clean_data <- subset(joined_data, joined_data$days_since_sticker < 0 | is.na(joi
 
 clean_data$n_stickers_installed <- if_else(is.na(clean_data$n_stickers_installed), 0, clean_data$n_stickers_installed)
 
+clean_data <- subset(clean_data, clean_data$sticker_installed == "TRUE")
+
+# Subset the data frame by column index numbers
+clean_data <- clean_data[, c(1:5, 7, 9:12, 74:81)]
+
+# Collapse the data frame by reg_no and calculate the average of x, y, and z
+agg_df <- aggregate(clean_data[, 12:17], by = list(reg_no = clean_data$reg_no), FUN = mean)
 
 
 ## Box plots by number of stickers
 
 # Proportion of time over 80km/h
-ggplot(clean_data, aes(
+plot_1 <- ggplot(clean_data, aes(
   x = as.factor(clean_data$n_stickers_installed), y = clean_data$prop_80,
   colour = as.factor(n_stickers_installed), alpha = 0.1
 )) +
@@ -91,6 +98,9 @@ ggplot(clean_data, aes(
     name = "Number of Stickers",
     labels = c("None", "Two", "Three", "Four", "Five", "Six")
   )
+
+plot_1
+ggplotly(plot_1)
 
 # Proportion of time over 100km/h
 
@@ -111,7 +121,7 @@ ggplot(clean_data, aes(
   )
 
 
-# G force violations per adsf
+# G force violations per hour
 
 ggplot(clean_data, aes(
   x = as.factor(clean_data$n_stickers_installed), y = clean_data$total_g_violations_per_hour,
@@ -128,3 +138,66 @@ ggplot(clean_data, aes(
     name = "Number of Stickers",
     labels = c("None", "Two", "Three", "Four", "Five", "Six")
   )
+
+
+
+#### Repeat analysis by regno id ####
+
+## Box plots by number of stickers
+
+# Proportion of time over 80km/h
+plot_1 <- ggplot(agg_df, aes(
+  x = as.factor(agg_df$n_stickers_installed), y = agg_df$prop_80,
+  colour = as.factor(n_stickers_installed), alpha = 0.1
+)) +
+  geom_boxplot(outlier.shape = NA) +
+  geom_jitter() +
+  coord_flip() +
+  xlab("Number of Stickers Installed") +
+  ylab("Proportion of time spent >80km/h") +
+  ggtitle("Testing bias in number of stickers") +
+  theme_bw() +
+  scale_color_discrete(
+    name = "Number of Stickers",
+    labels = c("None", "Two", "Three", "Four", "Five", "Six")
+  )
+
+plot_1
+
+# Proportion of time over 100km/h
+
+ggplot(agg_df, aes(
+  x = as.factor(agg_df$n_stickers_installed), y = agg_df$prop_100,
+  colour = as.factor(n_stickers_installed), alpha = 0.1
+)) +
+  geom_boxplot(outlier.shape = NA) +
+  geom_jitter() +
+  coord_flip() +
+  xlab("Number of Stickers Installed") +
+  ylab("Proportion of time spent >100 km/h") +
+  ggtitle("Testing bias in number of stickers") +
+  theme_bw() +
+  scale_color_discrete(
+    name = "Number of Stickers",
+    labels = c("None", "Two", "Three", "Four", "Five", "Six")
+  )
+
+
+# G force violations per hour
+
+ggplot(agg_df, aes(
+  x = as.factor(agg_df$n_stickers_installed), y = agg_df$total_g_violations_per_hour,
+  colour = as.factor(n_stickers_installed), alpha = 0.1
+)) +
+  geom_boxplot(outlier.shape = NA) +
+  geom_jitter() +
+  coord_flip() +
+  xlab("Number of Stickers Installed") +
+  ylab("Total G Force Violations Per Hour") +
+  ggtitle("Testing bias in number of stickers") +
+  theme_bw() +
+  scale_color_discrete(
+    name = "Number of Stickers",
+    labels = c("None", "Two", "Three", "Four", "Five", "Six")
+  )
+
